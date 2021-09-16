9b1765fcc841f4aeecec4bf24834d59fc2bb2237
Martin Czygan <martin.czygan@gmail.com>
2021-09-16 14:29:47 +0200

    docs: how to import settings

--- labe/settings.py
+++ labe/settings.py
@@ -3,6 +3,10 @@ Setting up settings, living under XDG paths.
 
 More information: https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 
+To access, import this module:
+
+    from labe.settings import settings
+
 """
 
 from pathlib import Path
