0bf9f2692a7351306cf82aa17ea0a1cd57efd533
Martin Czygan <martin.czygan@gmail.com>
2021-09-18 01:34:50 +0200

    doisniffer: add processing options

--- data/doisniffer.py
+++ data/doisniffer.py
@@ -40,21 +40,22 @@ Slow, about 2 krps.
 import argparse
 import collections
 import fileinput
-import json
+import os
 import re
 import sys
 
 import marcx
+import orjson as json
 
 Match = collections.namedtuple("Match", "key value match")
 
 
-def field_match(key, value, pattern):
+def field_match(key, value, pattern, parse_marc=True):
     """
     Given a root document, all matches of pattern per field. Also parse
     "fullrecord" into MARC21 and search each field for the pattern.
     """
-    if key == "fullrecord":
+    if key == "fullrecord" and parse_marc:
         try:
             record = marcx.FatRecord(data=value.encode("utf-8"))
         except UnicodeDecodeError as exc:
@@ -70,12 +71,59 @@ def field_match(key, value, pattern):
             yield Match(key, value, match)
     elif isinstance(value, list):
         for v in value:
-            yield from field_match(key, v, pattern)
+            yield from field_match(key, v, pattern, parse_marc=parse_marc)
     elif isinstance(value, dict):
         for k, v in value.items():
-            yield from field_match(k, v, pattern)
+            yield from field_match(k, v, pattern, parse_marc=parse_marc)
     else:
         yield None
+
+
+def sniff_doi(
+    fobj, writer=sys.stdout, aggressive=False, tab=True, update=False, parse_marc=True
+):
+    """
+    Sniff out DOI in a newline delimited JSON file. Output to stdout. If
+    aggressive is True, try to filter out invalid DOI more aggressively.
+    Arguments tab and update control whether we want to list the DOI or update
+    the document.
+    """
+    # Q: does a DOI allow slashes in the non-prefix, e.g. "10.123/abc/epdf"
+    # A: yes, rare, but that's ok! 10.6094/UNIFR/13040, 10.1051/hel/2019018, ...
+    pat_doi = re.compile(r'10[.][0-9]{2,6}/[^ "]{3,}')
+
+    for i, line in enumerate(fobj):
+        doc = json.loads(line)
+        sniffed = set()
+        for match in field_match(None, doc, pat_doi, parse_marc=parse_marc):
+            if not match:
+                continue
+            value = match.match.group(0)
+            # some exceptions
+            if value.endswith("/epdf"):
+                value = value[: len(value) - 5]
+            if value.endswith(".") or value.endswith("*"):
+                # 10.1007/978-3-322-84738-6.
+                # 10.4028/www.scientific.net/AMR.429*
+                value = value[:-1]
+            # can rule out barcode directly
+            if aggressive and ("barcode" in match.key or "dewey" in match.key):
+                print("[skip] {}:{}".format(match.key, value), file=sys.stderr)
+                sniffed = set()
+                break
+            sniffed.add(value)
+            if tab:
+                writer.write("{}\t{}\t{}\n".format(doc["id"], match.key, value,))
+        if update:
+            # <dynamicField name="*_str_mv" type="string" indexed="true"
+            # stored="true" multiValued="true" docValues="true"/>
+            if len(sniffed) > 1:
+                print(
+                    "[warn] multiple doi: {}".format(", ".join(sniffed)),
+                    file=sys.stderr,
+                )
+            doc["doi_str_mv"] = list(sniffed)
+            writer.write(json.dumps(doc) + "\n")
 
 
 if __name__ == "__main__":
@@ -92,41 +140,19 @@ if __name__ == "__main__":
     parser.add_argument(
         "--aggressive", action="store_true", help="filter out mostly like bad doi"
     )
+    parser.add_argument(
+        "-m", "--parse-marc", action="store_true", help="parse fullrecord as marc"
+    )
+    parser.add_argument(
+        "-q", "--quiet", action="store_true", help="suppress any output"
+    )
     args = parser.parse_args()
-
-    # Q: does a DOI allow slashes in the non-prefix, e.g. "10.123/abc/epdf"
-    # A: yes, rare, but that's ok! 10.6094/UNIFR/13040, 10.1051/hel/2019018, ...
-    pat_doi = re.compile(r'10[.][0-9]{2,6}/[^ "]{3,}')
-
-    for i, line in enumerate(args.input_file):
-        doc = json.loads(line)
-        sniffed = set()
-        for match in field_match(None, doc, pat_doi):
-            if not match:
-                continue
-            value = match.match.group(0)
-            # Postprocess some values
-            if value.endswith("/epdf"):
-                value = value[: len(value) - 5]
-            if value.endswith(".") or value.endswith("*"):
-                # 10.1007/978-3-322-84738-6.
-                # 10.4028/www.scientific.net/AMR.429*
-                value = value[:-1]
-            # can rule out barcode directly
-            if args.aggressive and ("barcode" in match.key or "dewey" in match.key):
-                print("[skip] {}:{}".format(match.key, value), file=sys.stderr)
-                sniffed = set()
-                break
-            sniffed.add(value)
-            if args.tab:
-                print("{}\t{}\t{}".format(doc["id"], match.key, value,))
-        if args.update:
-            # <dynamicField name="*_str_mv" type="string" indexed="true"
-            # stored="true" multiValued="true" docValues="true"/>
-            if len(sniffed) > 1:
-                print(
-                    "[warn] multiple doi: {}".format(", ".join(sniffed)),
-                    file=sys.stderr,
-                )
-            doc["doi_str_mv"] = list(sniffed)
-            print(json.dumps(doc))
+    writer = os.devnull if args.quiet else sys.stdout
+    sniff_doi(
+        args.input_file,
+        writer=writer,
+        aggressive=args.aggressive,
+        tab=args.tab,
+        update=args.update,
+        parse_marc=args.parse_marc,
+    )
