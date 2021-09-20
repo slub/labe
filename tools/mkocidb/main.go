de059d9da2e47d8b186ac3f49a3c64aeb353f2d0
Martin Czygan <martin.czygan@gmail.com>
2021-09-20 13:29:17 +0200

    wip: a chunked insert

--- tools/mkocidb/main.go
+++ tools/mkocidb/main.go
@@ -8,16 +8,32 @@
 // 2021/09/20 10:36:01 [2302] wrote 1048576 (2413821952)
 // 2021/09/20 10:36:01 copy failed: write |1: broken pipe
 //
+// A chucked insert seems to work.
+//
+// $ time ./mkocidb < sample-m.tsv
+// 2021/09/20 13:13:55 [ok] initialized database at data.db
+// written 5G -- 6.6M/s
+//
+// real    12m46.585s
+// user    11m17.193s
+// sys     3m2.369s
+//
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"flag"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
+	"sync"
+	"time"
 )
 
 var (
@@ -40,6 +56,12 @@ CREATE INDEX IF NOT EXISTS idx_k ON map(k);
 CREATE INDEX IF NOT EXISTS idx_v ON map(v);
 `
 	runSQL = `
+PRAGMA journal_mode = OFF;
+PRAGMA synchronous = 0;
+PRAGMA cache_size = 1000000;
+PRAGMA locking_mode = EXCLUSIVE;
+PRAGMA temp_store = MEMORY;
+
 .mode tabs
 .import /dev/stdin map
 `
@@ -58,6 +80,52 @@ func TempFileReader(r io.Reader) (string, error) {
 		return "", err
 	}
 	return f.Name(), nil
+}
+
+// ByteSize returns a human-readable byte string of the form 10M, 12.5K, and so
+// forth.  The following units are available: E: Exabyte, P: Petabyte, T:
+// Terabyte, G: Gigabyte, M: Megabyte, K: Kilobyte, B: Byte, The unit that
+// results in the smallest number greater than or equal to 1 is always chosen.
+func ByteSize(bytes int64) string {
+	const (
+		BYTE = 1 << (10 * iota)
+		KILOBYTE
+		MEGABYTE
+		GIGABYTE
+		TERABYTE
+		PETABYTE
+		EXABYTE
+	)
+	unit := ""
+	value := float64(bytes)
+	switch {
+	case bytes >= EXABYTE:
+		unit = "E"
+		value = value / EXABYTE
+	case bytes >= PETABYTE:
+		unit = "P"
+		value = value / PETABYTE
+	case bytes >= TERABYTE:
+		unit = "T"
+		value = value / TERABYTE
+	case bytes >= GIGABYTE:
+		unit = "G"
+		value = value / GIGABYTE
+	case bytes >= MEGABYTE:
+		unit = "M"
+		value = value / MEGABYTE
+	case bytes >= KILOBYTE:
+		unit = "K"
+		value = value / KILOBYTE
+	case bytes >= BYTE:
+		unit = "B"
+	case bytes == 0:
+		return "0B"
+	}
+
+	result := strconv.FormatFloat(value, 'f', 1, 64)
+	result = strings.TrimSuffix(result, ".0")
+	return result + unit
 }
 
 type LoggingWriter struct {
@@ -91,6 +159,34 @@ func setupDatabase(path string) error {
 	return nil
 }
 
+func runImport(r io.Reader, initFile, outputFile string) (int64, error) {
+	cmd := exec.Command("sqlite3", "--init", initFile, outputFile)
+	cmdStdin, err := cmd.StdinPipe()
+	if err != nil {
+		return 0, err
+	}
+	var (
+		wg      sync.WaitGroup
+		copyErr error
+		written int64
+	)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer cmdStdin.Close()
+		n, err := io.Copy(cmdStdin, r)
+		if err != nil {
+			copyErr = fmt.Errorf("copy failed: %w", err)
+		}
+		written += n
+	}()
+	if _, err := cmd.CombinedOutput(); err != nil {
+		return written, fmt.Errorf("exec failed: %w", err)
+	}
+	wg.Wait()
+	return written, copyErr
+}
+
 func main() {
 	flag.Parse()
 	if err := setupDatabase(*outputFile); err != nil {
@@ -100,33 +196,41 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// TODO: start one process every N lines to save memory (broke with "m" dataset, 100M rows)
-	cmd := exec.Command("sqlite3", "--init", runFile, *outputFile)
-	cmdStdin, err := cmd.StdinPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	go func() {
-		defer cmdStdin.Close()
-		br := buf.NewReader(os.Stdin)
-		for {
-			line, err := br.ReadString('\n')
-			if err == io.EOF {
-				break
-			}
+	var (
+		buf     bytes.Buffer
+		size    = 64 * 1 << 20
+		written int64
+		br      = bufio.NewReader(os.Stdin)
+		started = time.Now()
+	)
+	for {
+		b, err := br.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := buf.Write(b); err != nil {
+			log.Fatal(err)
+		}
+		if buf.Len() > size {
+			n, err := runImport(&buf, runFile, *outputFile)
 			if err != nil {
 				log.Fatal(err)
 			}
+			written += n
+			fmt.Printf(strings.Repeat(" ", 40))
+			fmt.Printf("\rwritten %s -- %s/s", ByteSize(written), ByteSize(int64(float64(written)/time.Since(started).Seconds())))
 		}
-		if _, err := io.CopyBuffer(cmdStdin, os.Stdin); err != nil {
-			log.Fatalf("copy failed: %v", err)
-		}
-	}()
-	if *dry {
-		log.Println(cmd)
-		os.Exit(0)
 	}
-	if _, err := cmd.CombinedOutput(); err != nil {
-		log.Fatalf("exec failed: %v", err)
+	n, err := runImport(&buf, runFile, *outputFile)
+	if err != nil {
+		log.Fatal(err)
 	}
+	written += n
+	fmt.Printf(strings.Repeat(" ", 40))
+	fmt.Printf("\rwritten %s -- %s/s", ByteSize(written), ByteSize(int64(float64(written)/time.Since(started).Seconds())))
+	fmt.Println()
+	log.Printf("db setup done")
 }
