61402944da7d31d24c2970fb24af6eb12ae22c51
Martin Czygan <martin.czygan@gmail.com>
2022-01-13 02:00:20 +0100

    cleanup unused code

--- go/ckit/cmd/labed/main.go
+++ go/ckit/cmd/labed/main.go
@@ -1,7 +1,8 @@
 // An API server for catalog and citation data.
 //
-// TODO: http: URL query contains semicolon, which is no longer a supported separator; parts of the query may be stripped when parsed; see golang.org/issue/25192
-// TODO: use globalconf for flags
+// TODO: http: URL query contains semicolon, which is no longer a supported
+// separator; parts of the query may be stripped when parsed; see
+// golang.org/issue/25192 TODO: use globalconf for flags
 
 //go:build linux
 
@@ -11,12 +12,11 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
 	"strings"
-	"syscall"
 	"time"
 
 	"github.com/gorilla/handlers"
@@ -28,11 +28,9 @@ import (
 )
 
 var (
-	identifierDatabasePath = flag.String("I", "i.db", "identifier database path")
-	ociDatabasePath        = flag.String("O", "o.db", "oci as a datbase path")
-	blobServerURL          = flag.String("bs", "", "blob server URL")
-	solrBlobPath           = flag.String("S", "", "solr blob URL")
-	listenAddr             = flag.String("l", "localhost:3000", "host and port to listen on")
+	identifierDatabasePath = flag.String("I", "", "identifier database path (id-doi mapping)")
+	ociDatabasePath        = flag.String("O", "", "oci as a datbase path (citations)")
+	listenAddr             = flag.String("l", "localhost:8000", "host and port to listen on")
 	enableStopWatch        = flag.Bool("W", false, "enable stopwatch")
 	enableGzip             = flag.Bool("z", false, "enable gzip compression")
 	enableLogging          = flag.Bool("L", false, "enable logging")
@@ -42,36 +40,37 @@ var (
 	cacheDefaultExpiration = flag.Duration("Cx", 72*time.Hour, "cache default expiration")
 	showVersion            = flag.Bool("version", false, "show version")
 	logFile                = flag.String("logfile", "", "file to log to")
+	quite                  = flag.Bool("q", false, "no output at all")
 
-	sqliteBlobPath xflag.Array // allows to specify multiple database to get catalog metadata from
+	sqliteFetcherPaths xflag.Array // allows to specify multiple database to get catalog metadata from
 
 	Version   string // set by makefile
 	Buildtime string // set by makefile
 	Help      string = `usage: labed [OPTION]
 
-labed is an api server for the labe project; it works with three data stores
-(sizes as of 01/2022):
+labed is an web service fusing Open Citations and Library Catalog data (SLUB);
+it works with three types of database files:
 
-* (1) [-I] an sqlite3 catalog id to doi translation database (around 13GB)
-* (2) [-O] an sqlite3 version of OCI/COCI (around 150GB)
-* (3) [-Q] a key-value mapping from catalog ids to catalog entities; these
-           can be repeated (size depends on how much metadata is included)
+(1) [-I] an sqlite3 catalog id-to-doi translation database (10+G)
+(2) [-O] an sqlite3 version of OCI/COCI (around 150+GB)
+(3) [-Q] an sqlite3 mapping from catalog ids to catalog entities; these
+         can be repeated (size depends on index size and on how much metadata is included)
 
 Each database may be updated separately, with separate processes.
 
 Examples
 
-- http://{{ .listenAddr }}/id/ai-49-aHR0cDovL2R4LmRvaS5vcmcvMTAuMTA3My9wbmFzLjg1LjguMjQ0NA
-- http://{{ .listenAddr }}/id/ai-49-aHR0cDovL2R4LmRvaS5vcmcvMTAuMTAwMS9qYW1hLjI4Mi4xNi4xNTE5
-- http://{{ .listenAddr }}/id/ai-49-aHR0cDovL2R4LmRvaS5vcmcvMTAuMTAwNi9qbXJlLjE5OTkuMTcxNQ
-- http://{{ .listenAddr }}/id/ai-49-aHR0cDovL2R4LmRvaS5vcmcvMTAuMTE3Ny8xMDQ5NzMyMzA1Mjc2Njg3
-- http://{{ .listenAddr }}/id/ai-49-aHR0cDovL2R4LmRvaS5vcmcvMTAuMTIxMC9qYy4yMDExLTAzODU
-- http://{{ .listenAddr }}/id/ai-49-aHR0cDovL2R4LmRvaS5vcmcvMTAuMTIxNC9hb3MvMTE3NjM0Nzk2Mw
-- http://{{ .listenAddr }}/id/ai-49-aHR0cDovL2R4LmRvaS5vcmcvMTAuMjMwNy8yMDk1NTIx
+  $ labed -C -z -l localhost:1234 -I i.db -O o.db -Q d.db
+
+  http://{{ .listenAddr }}/id/ai-49-aHR0cDovL2R4LmRvaS5vcmcvMTAuMTA3My9wbmFzLjg1LjguMjQ0NA
+  http://{{ .listenAddr }}/id/ai-49-aHR0cDovL2R4LmRvaS5vcmcvMTAuMTE3Ny8xMDQ5NzMyMzA1Mjc2Njg3
+  http://{{ .listenAddr }}/id/ai-49-aHR0cDovL2R4LmRvaS5vcmcvMTAuMTIxMC9qYy4yMDExLTAzODU
+  http://{{ .listenAddr }}/id/ai-49-aHR0cDovL2R4LmRvaS5vcmcvMTAuMTIxNC9hb3MvMTE3NjM0Nzk2Mw
+  http://{{ .listenAddr }}/id/ai-49-aHR0cDovL2R4LmRvaS5vcmcvMTAuMjMwNy8yMDk1NTIx
 
 Bulk requests
 
-    $ curl -sL https://git.io/JzVmJ |
+  $ curl -sL https://is.gd/xGqzsg | zstd -dc -T0 |
     parallel -j 40 "curl -s http://{{ .listenAddr }}/id/{}" |
     jq -rc '[.id, .doi, .extra.citing_count, .extra.cited_count, .extra.took] | @tsv'
 
@@ -100,7 +99,7 @@ Examples
 )
 
 func main() {
-	flag.Var(&sqliteBlobPath, "Q", "blob sqlite3 server (repeatable)")
+	flag.Var(&sqliteFetcherPaths, "Q", "index metadata cache sqlite3 path (repeatable)")
 	flag.Usage = func() {
 		fmt.Printf(strings.Replace(Help, `{{ .listenAddr }}`, *listenAddr, -1))
 		fmt.Println("Flags")
@@ -108,51 +107,51 @@ func main() {
 		flag.PrintDefaults()
 	}
 	flag.Parse()
+	// Show version.
 	if *showVersion {
 		fmt.Printf("labed %v %v\n", Version, Buildtime)
 		os.Exit(0)
 	}
-	var logWriter io.Writer = os.Stdout
-	if *logFile != "" {
-		f, err := os.OpenFile(*logFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-		if err != nil {
-			log.Fatalf("could not open log file: %v", err)
+	var (
+		logWriter                       io.Writer = os.Stdout
+		identifierDatabase, ociDatabase *sqlx.DB
+		fetcher                         ckit.Fetcher
+		err                             error
+	)
+	// Setup logging and log output.
+	switch {
+	case *quite:
+		log.SetOutput(ioutil.Discard)
+	default:
+		if *logFile != "" {
+			f, err := os.OpenFile(*logFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Fatalf("could not open log file: %v", err)
+			}
+			defer f.Close()
+			logWriter = f
 		}
-		defer f.Close()
-		logWriter = f
+		log.SetOutput(logWriter)
 	}
-	log.SetOutput(logWriter)
 	// Setup database connections.
-	if _, err := os.Stat(*identifierDatabasePath); os.IsNotExist(err) {
+	if identifierDatabase, err = openDatabase(*identifierDatabasePath); err != nil {
 		log.Fatal(err)
 	}
-	if _, err := os.Stat(*ociDatabasePath); os.IsNotExist(err) {
-		log.Fatal(err)
-	}
-	identifierDatabase, err := sqlx.Open("sqlite3", ckit.WithReadOnly(*identifierDatabasePath))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ociDatabase, err := sqlx.Open("sqlite3", ckit.WithReadOnly(*ociDatabasePath))
-	if err != nil {
+	if ociDatabase, err = openDatabase(*ociDatabasePath); err != nil {
 		log.Fatal(err)
 	}
 	// Setup index data fetcher.
-	var fetcher ckit.Fetcher
 	switch {
-	case *solrBlobPath != "":
-		fetcher = &ckit.SolrBlob{BaseURL: *solrBlobPath}
-	case *blobServerURL != "":
-		fetcher = &ckit.BlobServer{BaseURL: *blobServerURL}
-	case len(sqliteBlobPath) > 0:
-		group := &ckit.FetchGroup{}
-		if err := group.FromDatabaseFiles(sqliteBlobPath...); err != nil {
+	case len(sqliteFetcherPaths) > 0:
+		g := &ckit.FetchGroup{}
+		if err := g.FromFiles(sqliteFetcherPaths...); err != nil {
 			log.Fatal(err)
 		}
-		fetcher = group
-		log.Printf("setup group fetcher over %d databases: %v", len(group.Backends), group.Backends)
+		fetcher = g
+		log.Printf("setup group fetcher over %d databases: %v",
+			len(g.Backends), sqliteFetcherPaths)
 	default:
-		log.Fatal("need blob server (-bs), sqlite3 database (-Q) or solr (-S)")
+		log.Fatal("need sqlite3 metadata index database (-Q)")
 	}
 	// Setup server.
 	srv := &ckit.Server{
@@ -182,14 +181,14 @@ func main() {
 	if *enableLogging {
 		h = handlers.LoggingHandler(logWriter, h)
 	}
-	// Setup signal handler; TODO: actually reload connections.
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, syscall.SIGHUP)
-	go func() {
-		for sig := range c {
-			println(sig)
-			log.Printf("TODO: reloading database connections")
-		}
-	}()
 	log.Fatal(http.ListenAndServe(*listenAddr, h))
+}
+
+// openDatabase first ensures a file does actually exists, then create as
+// read-only database connection.
+func openDatabase(filename string) (*sqlx.DB, error) {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		return nil, fmt.Errorf("file not found: %s", filename)
+	}
+	return sqlx.Open("sqlite3", ckit.WithReadOnly(filename))
 }
