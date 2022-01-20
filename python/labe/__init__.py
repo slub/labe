53762d39f2c5b306da102e682d4effcd3efc6cde
Martin Czygan <martin.czygan@gmail.com>
2022-01-20 11:46:26 +0100

    v0.1.6

--- python/labe/__init__.py
+++ python/labe/__init__.py
@@ -2,4 +2,4 @@
 The labe project contains code for handling and serving citation data from Open
 Citation for the Saxon State and University Library Dresden (SLUB).
 """
-__version__ = "0.1.5"
+__version__ = "0.1.6"
