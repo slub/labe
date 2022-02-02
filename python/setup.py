a210ddead9257c6f8bc0349637f8d077839549b5
Martin Czygan <martin.czygan@gmail.com>
2022-02-02 13:30:28 +0100

    setup: fix dependency

--- python/setup.py
+++ python/setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r") as fh:
         entry_points={"console_scripts": ["labe=labe.cli:main"]},
         install_requires=[
             "luigi>=3,<4",
-            "pandas>=1.4,<2",
+            "pandas>=1.3.0,<2",
             "requests>=2.26,<3",
             "xdg>=5,<6",
         ],
