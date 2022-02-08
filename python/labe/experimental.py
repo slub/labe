c5cbda2fa96aecd2a60bfc369cd0bb71e2cf35d1
Martin Czygan <martin.czygan@gmail.com>
2022-02-08 12:56:34 +0100

    exp: stats tasks

--- python/labe/experimental.py
+++ python/labe/experimental.py
@@ -15,8 +15,11 @@ __all__ = [
     'ExpCombinedCitationsTable',
 ]
 
+class ExpTask(Task):
+    TAG = 'exp'
 
-class ExpRefcatDownload(Task):
+
+class ExpRefcatDownload(ExpTask):
     """
     Download refcat v2; over 21h.
     """
@@ -36,10 +39,11 @@ class ExpRefcatDownload(Task):
         self.create_symlink(name="current")
 
 
-class OpenCitationsDOITable(Task):
+class OpenCitationsDOITable(ExpTask):
     """
     DOI to DOI table, sorted.
     """
+    exp = luigi.Parameter(default="1", description="experiment id")
 
     def requires(self):
         return OpenCitationsSingleFile()
@@ -56,23 +60,22 @@ class OpenCitationsDOITable(Task):
         luigi.LocalTarget(output).move(self.output().path)
 
     def output(self):
-        fingerprint = self.open_citations_url_hash()
-        filename = "{}.tsv.zst".format(fingerprint)
-        return luigi.LocalTarget(path=self.path(filename=filename))
+        return luigi.LocalTarget(path=self.path())
 
     def on_success(self):
         self.create_symlink(name="current")
 
 
-class ExpRefcatOnly(Task):
+class ExpRefcatOnly(ExpTask):
     """
     Refcat only DOI-DOI pairs.
     """
+    exp = luigi.Parameter(default="1", description="experiment id")
 
     def requires(self):
         return {
             "refcat": ExpRefcatDownload(),
-            "oci": OpenCitationsDOITable(),
+            "oci": OpenCitationsDOITable(exp=self.exp),
         }
 
     def run(self):
@@ -85,20 +88,21 @@ class ExpRefcatOnly(Task):
         luigi.LocalTarget(output).move(self.output().path)
 
     def output(self):
-        return luigi.LocalTarget(path=self.path(ext="tsv.zst", digest=True), format=Zstd)
+        return luigi.LocalTarget(path=self.path(ext="tsv.zst"), format=Zstd)
 
     def on_success(self):
         self.create_symlink(name="current")
 
 
-class ExpOpenCitationsOnly(Task):
+class ExpOpenCitationsOnly(ExpTask):
     """
     OCI only DOI-DOI pairs.
     """
+    exp = luigi.Parameter(default="1", description="experiment id")
 
     def requires(self):
         return {
-            "oci": OpenCitationsDOITable(),
+            "oci": OpenCitationsDOITable(exp=self.exp),
             "refcat": ExpRefcatDownload(),
         }
 
@@ -112,13 +116,13 @@ class ExpOpenCitationsOnly(Task):
         luigi.LocalTarget(output).move(self.output().path)
 
     def output(self):
-        return luigi.LocalTarget(path=self.path(ext="tsv.zst", digest=True), format=Zstd)
+        return luigi.LocalTarget(path=self.path(ext="tsv.zst"), format=Zstd)
 
     def on_success(self):
         self.create_symlink(name="current")
 
 
-class ExpCombinedCitationsTable(Task):
+class ExpCombinedCitationsTable(ExpTask):
     """
     OCI and refcat as a single file with duplicates removed; in 02/2022 we
     gather 1,565,087,463 edges (but many from refcat are dataset related;
@@ -130,6 +134,8 @@ class ExpCombinedCitationsTable(Task):
     As is, stats generation keys on the URL of the oci dump, so it's a bit
     unwieldy to evaluate this dataset directly.
     """
+    exp = luigi.Parameter(default="1", description="experiment id")
+
 
     def requires(self):
         return {
@@ -149,7 +155,218 @@ class ExpCombinedCitationsTable(Task):
         luigi.LocalTarget(output).move(self.output().path)
 
     def output(self):
-        return luigi.LocalTarget(path=self.path(ext="tsv.zst", digest=True), format=Zstd)
+        return luigi.LocalTarget(path=self.path(ext="tsv.zst"), format=Zstd)
 
     def on_success(self):
         self.create_symlink(name="current")
+
+
+class ExpCitationsSourceDOI(ExpTask):
+    """
+    List of DOI that are source of a citation edge. Normalized and sorted.
+    18m54.167s.
+
+    Issues: We still have '"' in DOI.
+    """
+    exp = luigi.Parameter(default="1", description="experiment id")
+
+    def requires(self):
+        return ExpCombinedCitationsTable(exp=self.exp)
+
+    def run(self):
+        output = shellout("""
+                          zstdcat -T0 {input} |
+                          LC_ALL=C cut -f 1 |
+                          LC_ALL=C tr [:upper:] [:lower:] |
+                          LC_ALL=C sort -S50% |
+                          zstd -c -T0 > {output}
+                          """,
+                          input=self.input().path)
+        luigi.LocalTarget(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext="tsv.zst"), format=Zstd)
+
+    def on_success(self):
+        self.create_symlink(name="current")
+
+
+class OpenCitationsTargetDOI(ExpTask):
+    """
+    List of DOI that are target of a citation edge. Normalized and sorted.
+    """
+    exp = luigi.Parameter(default="1", description="experiment id")
+
+    def requires(self):
+        return OpenCitationsSingleFile()
+
+    def run(self):
+        output = shellout("""
+                          zstdcat -T0 {input} |
+                          LC_ALL=C cut -f 2 |
+                          LC_ALL=C tr [:upper:] [:lower:] |
+                          LC_ALL=C sort -S50% |
+                          zstd -c -T0 > {output}
+                          """,
+                          input=self.input().path)
+        luigi.LocalTarget(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext="tsv.zst"), format=Zstd)
+
+    def on_success(self):
+        self.create_symlink(name="current")
+
+
+class OpenCitationsCitedCount(ExpTask):
+    """
+    Generate a table with two columns: inbound link count and DOI.
+    """
+    exp = luigi.Parameter(default="1", description="experiment id")
+
+    def requires(self):
+        return OpenCitationsTargetDOI(exp=self.exp)
+
+    def run(self):
+        output = shellout(r"""
+                          zstdcat -T0 {input} |
+                          LC_ALL=C uniq -c |
+                          LC_ALL=C sort -S 40% -nr |
+                          LC_ALL=C sed -e 's@^[ ]*@@;s@ @\t@' |
+                          zstd -c -T0 > {output}
+                          """,
+                          input=self.input().path)
+        luigi.LocalTarget(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext="tsv.zst"), format=Zstd)
+
+    def on_success(self):
+        self.create_symlink(name="current")
+
+
+class OpenCitationsCitingCount(ExpTask):
+    """
+    Generate a table with two columns: outbound link count and DOI.
+    """
+    exp = luigi.Parameter(default="1", description="experiment id")
+
+    def requires(self):
+        return OpenCitationsSourceDOI(exp=self.exp)
+
+    def run(self):
+        output = shellout(r"""
+                          zstdcat -T0 {input} |
+                          LC_ALL=C uniq -c |
+                          LC_ALL=C sort -S 40% -nr |
+                          LC_ALL=C sed -e 's@^[ ]*@@;s@ @\t@' |
+                          zstd -c -T0 > {output}
+                          """,
+                          input=self.input().path)
+        luigi.LocalTarget(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext="tsv.zst"), format=Zstd)
+
+    def on_success(self):
+        self.create_symlink(name="current")
+
+
+class OpenCitationsInboundStats(ExpTask):
+    """
+    Inbound edge count distribution.
+
+      {
+        "inbound_edges": {
+          "count": 58110382,
+          "mean": 21.8783773612,
+          "std": 104.0590729122,
+          "min": 1,
+          "0%": 1,
+          "10%": 1,
+          "25%": 2,
+          "50%": 7,
+          "75%": 20,
+          "95%": 81,
+          "99%": 220,
+          "99.9%": 802,
+          "100%": 200934,
+          "max": 200934
+        }
+      }
+    """
+    exp = luigi.Parameter(default="1", description="experiment id")
+
+    def requires(self):
+        return OpenCitationsCitedCount(exp=self.exp)
+
+    def run(self):
+        output = shellout("zstdcat -T0 {input} | cut -f1 > {output}", input=self.input().path)
+        df = pd.read_csv(output, header=None, names=["inbound_edges"], skip_blank_lines=True)
+        percentiles = [0, 0.1, 0.25, 0.5, 0.75, 0.95, 0.99, 0.999, 1]
+        with tempfile.NamedTemporaryFile(mode="wb", delete=False) as f:
+            df.describe(percentiles=percentiles).to_json(f)
+        luigi.LocalTarget(f.name).move(self.output().path)
+        os.remove(output)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext="json"))
+
+    def on_success(self):
+        self.create_symlink(name="current")
+
+
+class OpenCitationsOutboundStats(Task):
+    """
+    Outbound edge count distribution.
+    """
+    exp = luigi.Parameter(default="1", description="experiment id")
+
+    def requires(self):
+        return OpenCitationsCitingCount(exp=self.exp)
+
+    def run(self):
+        output = shellout("zstdcat -T0 {input} | cut -f1 > {output}", input=self.input().path)
+        df = pd.read_csv(output, header=None, names=["outbound_edges"], skip_blank_lines=True)
+        percentiles = [0, 0.1, 0.25, 0.5, 0.75, 0.95, 0.99, 0.999, 1]
+        with tempfile.NamedTemporaryFile(mode="wb", delete=False) as f:
+            df.describe(percentiles=percentiles).to_json(f)
+        luigi.LocalTarget(f.name).move(self.output().path)
+        os.remove(output)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path(ext="json"))
+
+    def on_success(self):
+        self.create_symlink(name="current")
+
+
+class OpenCitationsUniqueDOI(Task):
+    """
+    List of unique DOI in citation dataset.
+    """
+    exp = luigi.Parameter(default="1", description="experiment id")
+
+    def requires(self):
+        return {
+            "s": OpenCitationsSourceDOI(exp=self.exp),
+            "t": OpenCitationsTargetDOI(exp=self.exp),
+        }
+
+    def run(self):
+        output = shellout("""
+                          LC_ALL=C sort -u -S 50%
+                            <(zstdcat -T0 {s} | LC_ALL=C uniq)
+                            <(zstdcat -T0 {t} | LC_ALL=C uniq)
+                          | zstd -c -T0 > {output}
+                          """,
+                          s=self.input().get("s").path,
+                          t=self.input().get("t").path)
+        luigi.LocalTarget(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path("tsv.zst"), format=Zstd)
+
+    def on_success(self):
+        self.create_symlink(name="current")
+
